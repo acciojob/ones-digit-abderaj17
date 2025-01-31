f57552de1a2b496d8df962776d45f3ed2437dc0c
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-31 19:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function onesDigit(n) {
   // your code here
 	let lastDigit = parseInt(n/10);
 	
-	return
+	return lastDigit;
 }
 
 // Do not change the code below
 
-// const n = prompt("Enter Number:");
-// alert(onesDigit(n));
+const n = prompt("Enter Number:");
+alert(onesDigit(n));
